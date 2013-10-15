0409ca09bb72acd8688062c8a7408238892b6e80
Becky Carella <becarella@gmail.com>
2013-10-15 16:13:56 -0400

    add durations to coupons

--- db/migrate/20130702193148_create_couponable_coupons.rb
+++ db/migrate/20130702193148_create_couponable_coupons.rb
@@ -1,21 +1,23 @@
 class CreateCouponableCoupons < ActiveRecord::Migration
   def change
-    create_table :couponable_coupons do |t|
-      t.string :name
-      t.string :code
-      t.integer :trial_duration
-      t.string :trial_duration_unit
-      t.integer :discount_amount_cents
-      t.string :discount_lifespan
-      t.integer :discount_lifespan_billing_cycles
-      t.integer :max_redemptions
-      t.datetime :expires_at
-      t.string :couponable_type
-      t.integer :couponable_id
-      t.string :type
+    if !ActiveRecord::Base.connection.table_exists? 'couponable_coupons'
+      create_table :couponable_coupons do |t|
+        t.string :name
+        t.string :code
+        t.integer :trial_duration
+        t.string :trial_duration_unit
+        t.integer :discount_amount_cents
+        t.string :discount_lifespan
+        t.integer :discount_lifespan_billing_cycles
+        t.integer :max_redemptions
+        t.datetime :expires_at
+        t.string :couponable_type
+        t.integer :couponable_id
+        t.string :type
 
-      t.timestamps
+        t.timestamps
+      end
+      add_index :couponable_coupons, :code, :unique => true
     end
-    add_index :couponable_coupons, :code, :unique => true
   end
 end
--- db/migrate/20130702195128_create_couponable_coupon_redemptions.rb
+++ db/migrate/20130702195128_create_couponable_coupon_redemptions.rb
@@ -1,16 +1,17 @@
 class CreateCouponableCouponRedemptions < ActiveRecord::Migration
   def change
-    create_table :couponable_coupon_redemptions do |t|
-      t.integer :coupon_id
-      t.integer :user_id
-      t.integer :coupon_redeemable_id
-      t.string  :coupon_redeemable_type
-
-      t.timestamps
+    if !ActiveRecord::Base.connection.table_exists? 'couponable_coupon_redemptions'
+      create_table :couponable_coupon_redemptions do |t|
+        t.integer :coupon_id
+        t.integer :user_id
+        t.integer :coupon_redeemable_id
+        t.string  :coupon_redeemable_type
+        t.timestamps
+      end
+      add_index :couponable_coupon_redemptions, :coupon_id
+      add_index :couponable_coupon_redemptions, :user_id
+      add_index :couponable_coupon_redemptions, :coupon_redeemable_id
+      add_index :couponable_coupon_redemptions, :coupon_redeemable_type
     end
-    add_index :couponable_coupon_redemptions, :coupon_id
-    add_index :couponable_coupon_redemptions, :user_id
-    add_index :couponable_coupon_redemptions, :coupon_redeemable_id
-    add_index :couponable_coupon_redemptions, :coupon_redeemable_type
   end
 end
