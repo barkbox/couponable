20df75669ea8b38a0e0e7c017cf75523ba474f1b
Becky Carella <becarella@gmail.com>
2013-10-15 15:29:30 -0400

    remove activeadmin and add bulk creation

--- app/models/couponable/coupon.rb
+++ app/models/couponable/coupon.rb
@@ -1,6 +1,6 @@
 class Couponable::Coupon < ActiveRecord::Base
   attr_accessible :code, :discount_amount_cents, :discount_lifespan, :discount_lifespan_billing_cycles,
-                  :expires_at, :max_redemptions, :name, :trial_duration, :trial_duration_unit
+                  :expires_at, :max_redemptions, :name, :trial_duration, :trial_duration_unit, :type
 
   belongs_to :couponable, :polymorphic => true
   has_many :coupon_redemptions
@@ -49,11 +49,23 @@ class Couponable::Coupon < ActiveRecord::Base
   def redemption_added
   end
 
+
   class << self
 
     def random_code prefix="", length=16
       charset = ('A'..'Z').to_a + (0..9).to_a
-      prefix + (0..(length-prefix.length)).collect{ charset[Random.rand(charset.length)] }.join
+      prefix + (0...(length-prefix.length)).collect{ charset[Random.rand(charset.length)] }.join
+    end
+    
+    def bulk_create count, options
+      code_length = options.delete(:code_length).to_i || 16
+      Couponable::Coupon.transaction do
+        count.times do
+          options[:code] = Couponable::Coupon.random_code(options[:prefix], code_length)
+          coupon = Couponable::Coupon.create(options)
+        end
+      end
+      
     end
   end
 
--- lib/couponable/engine.rb
+++ lib/couponable/engine.rb
@@ -1,11 +1,5 @@
 module Couponable
   class Engine < ::Rails::Engine
     isolate_namespace Couponable
-
-    initializer :couponable do
-      # ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '/admin']
-      ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/admin']
-    end
-
   end
 end
