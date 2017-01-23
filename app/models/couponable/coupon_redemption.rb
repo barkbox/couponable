e80cf6c91055e4f49362b4f4c8073f06051f9ffc
Dan Hunter <dan@thisishunter.com>
2017-01-23 16:53:57 -0500

    still using attr_accessible until we can remove protected_attributes gem

--- app/models/couponable/coupon_redemption.rb
+++ app/models/couponable/coupon_redemption.rb
@@ -1,6 +1,6 @@
 module Couponable
   class CouponRedemption < ActiveRecord::Base
-    # attr_accessible :coupon_id, :user_id, :coupon_redeemable
+    attr_accessible :coupon_id, :user_id, :coupon_redeemable
 
     belongs_to :coupon_redeemable, :polymorphic => true
     belongs_to :coupon, :class_name => 'Couponable::Coupon'
